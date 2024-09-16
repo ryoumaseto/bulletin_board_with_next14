65a326d59c612e7d8ff668b80f3ad3673b689ea5
ryoumaseto <ryohma919@gmail.com>
2024-09-16 12:32:39 +0900

    ミス

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,7 +1,11 @@
-import {NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 import prisma from '../../../lib/prismaClient';
 
 export async function GET(req: Request, res: Response) {
-    const allBBSPosts = await prisma.post.findMany();
+    const allBBSPosts = await prisma.post.findMany({
+        orderBy: {
+            id: 'asc',
+        },
+    });
     return NextResponse.json(allBBSPosts);
 }
